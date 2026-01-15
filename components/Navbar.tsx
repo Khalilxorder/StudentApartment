5dff328a8c337ae878d1ce43cbb4ab2f308a2364
Khalilkorder <169069309+Khalilxorder@users.noreply.github.com>
2026-01-15 19:34:39 +0100

    Rename home icon from Student Apartments to HOME

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -60,7 +60,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-2">
             <span className="text-2xl">🏠</span>
-            <span className="font-bold text-lg text-gray-900">Student Apartments</span>
+            <span className="font-bold text-lg text-gray-900">HOME</span>
           </Link>
 
           {/* Navigation Links */}
